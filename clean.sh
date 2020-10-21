d693702c826ae4c12a1833bb7c8e494501efc041
root <root@test.ru-central1.internal>
2020-10-21 11:47:50 +0000

    1

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#bin/bash
+docker stop $(docker ps)
+docker rm $(docker ps -a -f status=exited -q)
+#docker rm $(docker ps -a)
+docker rmi -f $(docker images -a -q)
+docker volume rm $(docker volume ls -q)
+docker ps -a
+docker images
+docker volume ls
