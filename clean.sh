9232deede8762101ad1b7196ebd73c279c8ce2a2
Enot41 <derick41@ya.ru>
2020-10-23 11:05:26 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #bin/bash
 docker stop $(docker ps)
 docker rm $(docker ps -a -f status=exited -q)
+docker rm $(docker ps -a -q)
 #docker rm $(docker ps -a)
 docker rmi -f $(docker images -a -q)
 docker volume rm $(docker volume ls -q)
